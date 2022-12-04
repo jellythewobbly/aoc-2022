17415eb05f6b10024e293913c4977358a6df1e4d
Jay Lee <39648852+jellythewobbly@users.noreply.github.com>
2022-12-05 00:18:35 +0800

    aoc-2022 day 4

---
+++ day-4/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := getInput()
+	p1 := part1(&input)
+	fmt.Printf("Part 1: %v\n", p1)
+
+	p2 := part2(&input)
+	fmt.Printf("Part 2: %v\n", p2)
+}
+
+func part1(input *[]string) int {
+	count := 0
+	for _, line := range *input {
+		split := strings.Split(line, ",")
+
+		a := split[0]
+		aSplit := strings.Split(a, "-")
+		aStart, _ := strconv.Atoi(aSplit[0])
+		aEnd, _ := strconv.Atoi(aSplit[1])
+
+		b := split[1]
+		bSplit := strings.Split(b, "-")
+		bStart, _ := strconv.Atoi(bSplit[0])
+		bEnd, _ := strconv.Atoi(bSplit[1])
+
+		if isFullyContain(aStart, aEnd, bStart, bEnd) {
+			count++
+		}
+	}
+	return count
+}
+
+func part2(input *[]string) int {
+	count := 0
+	for _, line := range *input {
+		split := strings.Split(line, ",")
+
+		a := split[0]
+		aSplit := strings.Split(a, "-")
+		aStart, _ := strconv.Atoi(aSplit[0])
+		aEnd, _ := strconv.Atoi(aSplit[1])
+
+		b := split[1]
+		bSplit := strings.Split(b, "-")
+		bStart, _ := strconv.Atoi(bSplit[0])
+		bEnd, _ := strconv.Atoi(bSplit[1])
+
+		if hasOverlap(aStart, aEnd, bStart, bEnd) {
+			count++
+		}
+	}
+	return count
+}
+
+func isFullyContain(aStart int, aEnd int, bStart int, bEnd int) bool {
+	diffA := bStart - aStart
+	diffB := bEnd - aEnd
+
+	var outStart, outEnd, inStart, inEnd int
+
+	if diffA < diffB {
+		outStart, outEnd, inStart, inEnd = bStart, bEnd, aStart, aEnd
+	} else {
+		outStart, outEnd, inStart, inEnd = aStart, aEnd, bStart, bEnd
+	}
+
+	if outStart <= inStart && outEnd >= inEnd {
+		return true
+	}
+	return false
+}
+
+func hasOverlap(aStart int, aEnd int, bStart int, bEnd int) bool {
+	if (aStart < bStart && aEnd < bStart) || (bStart < aStart && bEnd < aStart) {
+		return false
+	}
+	return true
+}
+
+func getInput() []string {
+	fileReader, err := os.Open("./input")
+	if err != nil {
+		fmt.Println("error in reading file")
+		return []string{}
+	}
+
+	defer fileReader.Close()
+
+	scanner := bufio.NewScanner(fileReader)
+
+	var res []string
+
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+	return res
+}
